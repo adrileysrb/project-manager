645b918bd0a6fe71f7575e62ea66657eb05c22f7
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2024-07-12 08:22:05 -0300

    feat: project-list component and setup primeng component library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { RouterModule } from '@angular/router';
+
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProjectListComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, ButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
